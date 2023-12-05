af6a2b8db588c330e3f4f51f712dcbfb7700da71
cadowtin <chasdowtin@gmail.com>
2023-12-05 12:24:32 -0600

    change quote type to fix it maybe?

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,6 @@
-import {debugLog} from './helpers/misc.js'
-import {generateDamageScroll, generateRollScroll, shakeScreen} from './helpers/anim.js'
-import {getDamageList} from './helpers/rollTerms.js'
+import {debugLog} from "./helpers/misc.js"
+import {generateDamageScroll, generateRollScroll, shakeScreen} from "./helpers/anim.js"
+import {getDamageList} from "./helpers/rollTerms.js"
 
 // HOOKS STUFF
 Hooks.on("ready", async () => {
