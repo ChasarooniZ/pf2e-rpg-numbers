c91e68047b1af2af583cbc2bde1e4c21d76eaac2
cadowtin <chasdowtin@gmail.com>
2023-12-20 03:15:31 -0600

    use object vs document

--- scripts/helpers/anim.js
+++ scripts/helpers/anim.js
@@ -323,7 +323,7 @@ export function damageShakeRollDamage(token, targets) {
 
 export function shakeOnDamageToken(actor_uuid) {
     let tok_uuid = actor_uuid.split('.').slice(0,-2).join('.');
-    const token = fromUuidSync(tok_uuid).token;
+    const token = fromUuidSync(tok_uuid).token.object;
     const { x: tok_x, y: tok_y, w: tok_width } = token;
     const shake_distance = 0.2;
     const shakes = 7
