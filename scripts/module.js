c5bc42b4cc2e49f7de02a2c1e89ae8957c23088b
cadowtin <chasdowtin@gmail.com>
2023-12-20 00:37:35 -0600

    Possible fix for no functionality

--- scripts/module.js
+++ scripts/module.js
@@ -3,44 +3,46 @@ import { generateDamageScroll, generateRollScroll, shakeScreen } from "./helpers
 import { getDamageList } from "./helpers/rollTerms.js"
 
 // HOOKS STUFF
-// Hooks.on("ready", async () => {
-    //console.error("PF2e RPG Numbers is ready");
+Hooks.once("ready", async () => {
+    console.log("PF2e RPG Numbers is starting");
     //ui.notifications.notify("PF2e RPG Numbers is ready")
     // game.RPGNumbers = new RPGNumbers();
-// })
-
-Hooks.on("createChatMessage", async function (msg, status, id) {
-    if (!game.settings.get("pf2e-rpg-numbers", 'enabled')) return;
-    debugLog({
-        msg
-    })
-    if (game.user.isGM) {
-        if (msg.isDamageRoll && game.settings.get("pf2e-rpg-numbers", 'dmg-enabled')) {
-            const dmg_list = getDamageList(msg.rolls);
-            const targets = getTargetList(msg);
-            debugLog({
-                targets,
-                dmg_list
-            })
-            generateDamageScroll(dmg_list, targets);
-        }
-        if (msg.isCheckRoll && game.settings.get("pf2e-rpg-numbers", 'check-enabled')) {
-            const roll_deets = {
-                outcome: msg.flags.pf2e.context.outcome ?? 'none',
-                token: msg.token,
-                whisper: msg.whisper,
-                roll: msg.rolls[0]?.total ?? '',
-                type: msg.flags.pf2e.context.type
+    Hooks.on("createChatMessage", async function (msg, status, id) {
+        if (!game.settings.get("pf2e-rpg-numbers", 'enabled')) return;
+        debugLog({
+            msg
+        })
+        if (game.user.isGM) {
+            if (msg.isDamageRoll && game.settings.get("pf2e-rpg-numbers", 'dmg-enabled')) {
+                const dmg_list = getDamageList(msg.rolls);
+                const targets = getTargetList(msg);
+                debugLog({
+                    targets,
+                    dmg_list
+                })
+                generateDamageScroll(dmg_list, targets);
             }
-            generateRollScroll(roll_deets);
+            if (msg.isCheckRoll && game.settings.get("pf2e-rpg-numbers", 'check-enabled')) {
+                const roll_deets = {
+                    outcome: msg.flags.pf2e.context.outcome ?? 'none',
+                    token: msg.token,
+                    whisper: msg.whisper,
+                    roll: msg.rolls[0]?.total ?? '',
+                    type: msg.flags.pf2e.context.type
+                }
+                generateRollScroll(roll_deets);
+            }
+            if (!!msg.flags?.pf2e?.appliedDamage && !msg.flags?.pf2e?.appliedDamage?.isHealing && game.settings.get("pf2e-rpg-numbers", 'shake-enabled')) {
+                let dmg = msg.flags.pf2e.appliedDamage.updates.find(u => u.path === "system.attributes.hp.value")?.value;
+                if (dmg)
+                    shakeScreen(msg.flags.pf2e.appliedDamage.uuid, dmg)
+            }
         }
-        if (!!msg.flags?.pf2e?.appliedDamage && !msg.flags?.pf2e?.appliedDamage?.isHealing && game.settings.get("pf2e-rpg-numbers", 'shake-enabled')) {
-            let dmg = msg.flags.pf2e.appliedDamage.updates.find(u => u.path === "system.attributes.hp.value")?.value;
-            if (dmg)
-                shakeScreen(msg.flags.pf2e.appliedDamage.uuid, dmg)
-        }
-    }
+    });
+
+    console.log("PF2e RPG Numbers is ready");
 })
+
 
 export function getTargetList(msg) {
     if (msg.flags?.["pf2e-target-damage"]?.targets) {
