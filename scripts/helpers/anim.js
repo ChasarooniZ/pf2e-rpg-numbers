506f83733733d2edd3bbb11a2c2a2bd7ab657798
cadowtin <chasdowtin@gmail.com>
2024-01-24 22:48:06 -0600

    FIX FOR SCALING DAMAGE shake better

--- scripts/helpers/anim.js
+++ scripts/helpers/anim.js
@@ -361,8 +361,7 @@ export function shakeOnDamageToken(actor_uuid, dmg) {
         .effect() //Make sure this only plays to people that can see it
         .atLocation(token)
         .file(token.document.texture.src)
-        .scaleToObject()
-        .spriteScale(token.document.texture.src.scaleX)
+        .scaleToObject(token.document.texture.scaleX)
         .loopProperty("spriteContainer", "position.x", { values, duration: it_dur, ease: 'easeInOutSine', pingPong: true })
         .duration(duration)
         .waitUntilFinished()
