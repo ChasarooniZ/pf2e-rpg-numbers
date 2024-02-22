12e80e6d8692f43806596ccdf9d11290a354bbaa
cadowtin <chasdowtin@gmail.com>
2024-02-22 13:08:30 -0600

    readded Debug setting

--- scripts/settings.js
+++ scripts/settings.js
@@ -428,9 +428,11 @@ Hooks.on("init", () => {
         name: game.i18n.localize("pf2e-rpg-numbers.module-settings.debug-mode.name"),
         hint: game.i18n.localize("pf2e-rpg-numbers.module-settings.debug-mode.hint"),
         scope: "world",
+        config: true,
         default: false,
         type: Boolean,
     });
+
     game.settings.register("pf2e-rpg-numbers", "updateMessage", {
         scope: "world",
         config: false,
