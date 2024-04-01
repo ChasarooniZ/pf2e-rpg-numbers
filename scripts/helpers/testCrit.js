2beabdf8264cb6b71b5d127087b2366747540367
cadowtin <chasdowtin@gmail.com>
2024-04-01 13:41:11 -0500

    test crit update pushed

--- scripts/helpers/testCrit.js
+++ scripts/helpers/testCrit.js
@@ -1,16 +1,17 @@
 import { createCritAnimation } from "./animation/crit-animation.js";
-
-document.getElementById("critButton").addEventListener("click", function (e) {
-    e.preventDefault();
-    ui.notifications.info("This is an info message");
-    const uuid = e.srcElement.parentElement.parentElement.parentElement.parentElement.id.replace(
-        "TokenConfigPF2e-",
-        ""
-    );
-    const token = await fromUuid(uuid);
-    console.log({
-        e,
-        test: "TESTANIM",
+export async function testCrit() {
+    document.getElementById("critButton").addEventListener("click", function (e) {
+        e.preventDefault();
+        ui.notifications.info("This is an info message");
+        const uuid = e.srcElement.parentElement.parentElement.parentElement.parentElement.id.replace(
+            "TokenConfigPF2e-",
+            ""
+        );
+        const token = await fromUuid(uuid);
+        console.log({
+            e,
+            test: "TESTANIM",
+        });
+        createCritAnimation({ type: "custom", whisper: [game.user.id], token: token ?? game.user.character });
     });
-    createCritAnimation({ type: "custom", whisper: [game.user.id], token: token ?? game.user.character });
-});
+}
