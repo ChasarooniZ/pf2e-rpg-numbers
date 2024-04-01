7d1c00b5512a90f23ade207c87c67e50aec3e1fa
cadowtin <chasdowtin@gmail.com>
2024-04-01 00:33:18 -0500

    Crit preview button work test

--- scripts/module.js
+++ scripts/module.js
@@ -122,10 +122,10 @@ Hooks.on("ready", () => {
             notes: game.i18n.localize("pf2e-rpg-numbers.token-options.crit.rotation.hint"),
             default: 0,
         },
-        // previewCrit: {
-        //     type: "custom",
-        //     html: '<button onclick="(e) => {ui.notifications.info("This is an info message");console.log({ event, test: "TESTANIM" });}">Test Crit Animation</button>',
-        // },
+        previewCrit: {
+            type: "custom",
+            html: await fetch('.../templates/previewCrit.html').then(response => response.text()),
+        },
     });
 
     if (game.user.isGM) {
