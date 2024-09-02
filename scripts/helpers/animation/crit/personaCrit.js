a6b3f93d67d7c6ba6244ccd8eaf20ff0d6e91d2d
Bullesta <62106319+Bullesta@users.noreply.github.com>
2024-09-02 04:33:40 -0600

    Added webm support for Persona Crits

--- scripts/helpers/animation/crit/personaCrit.js
+++ scripts/helpers/animation/crit/personaCrit.js
@@ -1,15 +1,14 @@
 import { getSetting} from "../../misc.js";
-
 /**
  *  Conversion method from css clip path
  * r = """0% 55%, 2% 52%, 9% 51%, 15% 44%, 23% 40%, 32% 38%, 34% 36%, 35% 35%, 41% 28%, 43% 30%, 50% 26%, 53% 27%, 58% 26%, 59% 26%, 62% 24%, 65% 25%, 71% 23%, 78% 15%, 85% 14%, 89% 14%, 95% 11%, 97% 12%, 100% 9%, 100% 55%, 97% 53%, 96% 55%, 92% 55%, 80% 56%, 72% 57%, 69% 58%, 64% 63%, 62% 63%, 61% 65%, 59% 63%, 57% 62%, 55% 64%, 53% 65%, 49% 63%, 43% 63%, 39% 64%, 37% 65%, 36% 65%, 34% 68%, 32% 67%, 29% 72%, 27% 71%, 27% 73%, 24% 72%, 22% 73%, 20% 70%, 16% 73%, 14% 71%, 13% 72%, 10% 71%, 5% 72%, 6% 70%, 0% 73%"""
- 
-for pair in r.split(","):
-    pair = pair.replace('%', '').strip()
-    items = pair.split(" ")
-    widthPer = str(int(items[0])/100)
-    heightPer = str(int(items[1])/100)
-    print("[" + widthPer + "* width, " + heightPer + "* height],")
+
+ for pair in r.split(","):
+ pair = pair.replace('%', '').strip()
+ items = pair.split(" ")
+ widthPer = str(int(items[0])/100)
+ heightPer = str(int(items[1])/100)
+ print("[" + widthPer + "* width, " + heightPer + "* height],")
  */
 //https://www.cssportal.com/css-clip-path-generator/
 /**
@@ -99,72 +98,141 @@ export function personaCrit(token, users, imgData, config) {
     ];
 
     const imageUrl = personaImg || imgData.img;
+    const isWebm = imageUrl.endsWith(".webm");
     const tokenScaler = ImageData.isToken ? (imgData.scaleX + imgData.yScale) / 2 : 1;
     const imageScaler = personaImg ? 1 : tokenScaler;
     const duration = getSetting("critical.duration") * 1000;
     const soundUrl = config.sfx;
     const volumeLevel = config.volume;
 
-    const image = new Image();
-    image.src = imageUrl;
+    if (isWebm) {
+        const video = document.createElement("video");
+        video.src = imageUrl;
+        video.autoplay = true;
+        video.loop = true;
+        video.muted = true;
 
-    image.onload = async ({ target }) => {
-        const imageHeight = target.height;
-        const imagePercent = (imageHeight * imageScaler) / 100;
+        video.onloadeddata = async () => {
+            const videoHeight = video.videoHeight;
+            const videoPercent = (videoHeight * imageScaler) / 100;
 
-        const scale = (critScale / 100) * (screenHeight / imageHeight) * imageScaler;
-        const offsetX = critOffsetX * imagePercent * scale;
-        const offsetY = (personaImg ? 0 : imagePercent * 20) + critOffsetY * imagePercent * scale;
+            const scale = (critScale / 100) * (screenHeight / videoHeight) * imageScaler;
+            const offsetX = critOffsetX * videoPercent * scale;
+            const offsetY = (personaImg ? 0 : videoPercent * 20) + critOffsetY * videoPercent * scale;
 
-        await Sequencer.Preloader.preloadForClients([imageUrl, soundUrl]);
-        await new Sequence()
-            //BG Color
-            .effect()
-            .syncGroup(`p5-crit-${token.uuid}`)
-            .shape("polygon", { points: centeredPoints, fillColor: game.user.color.css, fillAlpha: 1 })
-            .screenSpace()
-            .screenSpacePosition({ x: 0, y: 0 })
-            .screenSpaceAnchor({ x: 0.5, y: 0.5 })
-            .screenSpaceAboveUI()
-            .zIndex(-1)
-            .duration(duration)
-            .forUsers(users)
-            .delay(config.delay)
-            //Image
-            .effect()
-            .syncGroup(`p5-crit-${token.uuid}`)
-            .file(imageUrl)
-            .zIndex(0)
-            .shape("polygon", { isMask: true, points: centeredPoints })
-            .scale(typeof scale === "number" ? scale : 1)
-            .spriteOffset({ x: offsetX, y: offsetY }, { gridUnits: false })
-            .spriteRotation(critRotation)
-            .screenSpace()
-            .screenSpacePosition({ x: 0, y: 0 })
-            .screenSpaceAnchor({ x: 0.5, y: 0.5 })
-            .screenSpaceAboveUI()
-            .duration(duration)
-            .forUsers(users)
-            .delay(config.delay)
-            //Outline
-            .effect()
-            .syncGroup(`p5-crit-${token.uuid}`)
-            .zIndex(1)
-            .shape("polygon", { points: centeredPoints, fillAlpha: 0, lineSize: 10, lineColor: "white" })
-            .screenSpace()
-            .screenSpacePosition({ x: 0, y: 0 })
-            .screenSpaceAnchor({ x: 0.5, y: 0.5 })
-            .screenSpaceAboveUI()
-            .duration(duration)
-            .forUsers(users)
-            .delay(config.delay)
+            await Sequencer.Preloader.preloadForClients([imageUrl, soundUrl]);
+            await new Sequence()
+                // BG Color
+                .effect()
+                .syncGroup(`p5-crit-${token.uuid}`)
+                .shape("polygon", { points: centeredPoints, fillColor: game.user.color.css, fillAlpha: 1 })
+                .screenSpace()
+                .screenSpacePosition({ x: 0, y: 0 })
+                .screenSpaceAnchor({ x: 0.5, y: 0.5 })
+                .screenSpaceAboveUI()
+                .zIndex(-1)
+                .duration(duration)
+                .forUsers(users)
+                .delay(config.delay)
+                // Video
+                .effect()
+                .syncGroup(`p5-crit-${token.uuid}`)
+                .file(imageUrl)
+                .zIndex(0)
+                .shape("polygon", { isMask: true, points: centeredPoints })
+                .scale(typeof scale === "number" ? scale : 1)
+                .spriteOffset({ x: offsetX, y: offsetY }, { gridUnits: false })
+                .spriteRotation(critRotation)
+                .screenSpace()
+                .screenSpacePosition({ x: 0, y: 0 })
+                .screenSpaceAnchor({ x: 0.5, y: 0.5 })
+                .screenSpaceAboveUI()
+                .duration(duration)
+                .forUsers(users)
+                .delay(config.delay)
+                // Outline
+                .effect()
+                .syncGroup(`p5-crit-${token.uuid}`)
+                .zIndex(1)
+                .shape("polygon", { points: centeredPoints, fillAlpha: 0, lineSize: 10, lineColor: "white" })
+                .screenSpace()
+                .screenSpacePosition({ x: 0, y: 0 })
+                .screenSpaceAnchor({ x: 0.5, y: 0.5 })
+                .screenSpaceAboveUI()
+                .duration(duration)
+                .forUsers(users)
+                .delay(config.delay)
+                // Sound
+                .sound()
+                .file(soundUrl)
+                .fadeOutAudio(duration / 4)
+                .volume(volumeLevel)
+                .forUsers(users)
+                .delay(config.delay)
+                .play();
+        };
+    } else {
+        const image = new Image();
+        image.src = imageUrl;
 
-            .sound()
-            .file(soundUrl)
-            .fadeOutAudio(duration / 4)
-            .volume(volumeLevel)
-            .forUsers(users)
-            .delay(config.delay)
-            .play();
-    };
+        image.onload = async ({ target }) => {
+            const imageHeight = target.height;
+            const imagePercent = (imageHeight * imageScaler) / 100;
+
+            const scale = (critScale / 100) * (screenHeight / imageHeight) * imageScaler;
+            const offsetX = critOffsetX * imagePercent * scale;
+            const offsetY = (personaImg ? 0 : imagePercent * 20) + critOffsetY * imagePercent * scale;
+
+            await Sequencer.Preloader.preloadForClients([imageUrl, soundUrl]);
+            await new Sequence()
+                // BG Color
+                .effect()
+                .syncGroup(`p5-crit-${token.uuid}`)
+                .shape("polygon", { points: centeredPoints, fillColor: game.user.color.css, fillAlpha: 1 })
+                .screenSpace()
+                .screenSpacePosition({ x: 0, y: 0 })
+                .screenSpaceAnchor({ x: 0.5, y: 0.5 })
+                .screenSpaceAboveUI()
+                .zIndex(-1)
+                .duration(duration)
+                .forUsers(users)
+                .delay(config.delay)
+                // Image
+                .effect()
+                .syncGroup(`p5-crit-${token.uuid}`)
+                .file(imageUrl)
+                .zIndex(0)
+                .shape("polygon", { isMask: true, points: centeredPoints })
+                .scale(typeof scale === "number" ? scale : 1)
+                .spriteOffset({ x: offsetX, y: offsetY }, { gridUnits: false })
+                .spriteRotation(critRotation)
+                .screenSpace()
+                .screenSpacePosition({ x: 0, y: 0 })
+                .screenSpaceAnchor({ x: 0.5, y: 0.5 })
+                .screenSpaceAboveUI()
+                .duration(duration)
+                .forUsers(users)
+                .delay(config.delay)
+                // Outline
+                .effect()
+                .syncGroup(`p5-crit-${token.uuid}`)
+                .zIndex(1)
+                .shape("polygon", { points: centeredPoints, fillAlpha: 0, lineSize: 10, lineColor: "white" })
+                .screenSpace()
+                .screenSpacePosition({ x: 0, y: 0 })
+                .screenSpaceAnchor({ x: 0.5, y: 0.5 })
+                .screenSpaceAboveUI()
+                .duration(duration)
+                .forUsers(users)
+                .delay(config.delay)
+                // Sound
+                .sound()
+                .file(soundUrl)
+                .fadeOutAudio(duration / 4)
+                .volume(volumeLevel)
+                .forUsers(users)
+                .delay(config.delay)
+                .play();
+        };
+    }
 }
