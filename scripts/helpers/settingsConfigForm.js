1fd21cbf6049c104dad7a52a0a3217253791c427
cadowtin <chasdowtin@gmail.com>
2024-09-16 17:47:56 -0500

    extra

--- scripts/helpers/settingsConfigForm.js
+++ scripts/helpers/settingsConfigForm.js
@@ -92,10 +92,56 @@ export class SettingsConfigForm extends FormApplication {
                     }
                 },
                 rotateOnAttack: {
-                    fontSize: getSetting(""),
-                    fontSize: getSetting(""),
-                    fontSize: getSetting(""),
-                    fontSize: getSetting(""),
+                    enabled: getSetting("rotate-on-attack"),
+                    duration: getSetting("rotate-on-attack.duration"),
+                    scaleOnSize: getSetting("rotate-on-attack.scale-on-size"),
+                },
+                critical: {
+                    critical: getSetting("critical.enabled"),
+                    style: getSetting("critical.type"),
+                    checksOrAttacks: getSetting("critical.show-on"),
+                    pcOrNPC: getSetting("critical.show-on-token-type"),
+                    defImage: getSetting("critical.default-img"),
+                    duration: getSetting("critical.duration"),
+                    sound: getSetting("critical.sound"),
+                    volume: getSetting("critical.volume"),
+                    delay: getSetting("critical.delay"),
+                },
+                finishingMove: {
+                    enabled: getSetting("finishing-move.enabled"),
+                    enabledPlayers: getSetting("finishing-move.enabled-players"),
+                    keepOn: getSetting("finishing-move.keep-on"),
+                    usePlayerColor: getSetting("finishing-move.use-player-color"),
+                    quality: getSetting("finishing-move.quality"),
+                    sound: getSetting("finishing-move.sound-effect"),
+                    volume: getSetting("finishing-move.sound-effect.volume"),
+                    duration: {
+                        word: getSetting("finishing-move.duration.word"),
+                        end: getSetting("finishing-move.duration.end"),
+                    },
+                },
+                fromSoftware: {
+                    eldenRing: {
+                        nounVerbed: {
+                            enabled: getSetting("from-software.nounVerbed.enabled"),
+                            fontSize: getSetting("from-software.nounVerbed.font-size"),
+                            sound: getSetting("from-software.nounVerbed.sound-effect"),
+                            volume: getSetting("from-software.nounVerbed.sound-effect.volume"),
+                            duration: getSetting("from-software.nounVerbed.duration"),
+                            text: getSetting("text"),
+                        },
+                        death: {
+                            enabled: getSetting("from-software.death.enabled"),
+                            fontSize: getSetting("from-software.death.font-size"),
+                            sound: getSetting("from-software.death.sound-effect"),
+                            volume: getSetting("from-software.death.sound-effect.volume"),
+                            duration: getSetting("from-software.death.duration"),
+                            text: getSetting("from-software.death.text"),
+                        }
+                    }
+                },
+                debug: {
+                    enabled: getSetting("debug-mode"),
                 }
             },
             tabs: [
