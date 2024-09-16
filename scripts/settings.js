8480419422995c52889f8e3407d09dc389eb06d9
cadowtin <chasdowtin@gmail.com>
2024-09-16 00:44:39 -0500

    Renamed

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,5 @@
 import { localize, MODULE_ID, registerSetting } from "./helpers/misc.js";
-import { SettingsConfigForm } from "./helpers/settingsConfigForm.js";
+import { SettingsConfigForm } from "./helpers/test.js";
 
 Hooks.on("init", () => {
     loadTemplates([`modules/pf2e-rpg-numbers/templates/config.html`])
