ec8165f376022c6e0575ad578229886b4dd86b0b
cadowtin <chasdowtin@gmail.com>
2024-09-17 14:20:44 -0500

    fixing import

--- scripts/settings.js
+++ scripts/settings.js
@@ -2,7 +2,7 @@ import { localize, MODULE_ID, registerSetting } from "./helpers/misc.js";
 import { SettingsConfigForm } from "./helpers/settingsConfigForm.js";
 
 Hooks.on("init", () => {
-    loadTemplates([`modules/pf2e-rpg-numbers/templates/pf2e-rpg-settings-config.hbs`])
+    loadTemplates([`modules/pf2e-rpg-numbers/templates/pf2e-rpg-settings-config.hbs`, 'modules/pf2e-rpg-numbers/templates/settings-tabs/main-tab.hbs'])
     Hooks.on("renderSettingsConfig", renderSettingsConfig);
     //TODO finalize this
     // game.settings.registerMenu(mod, 'settingsMenu', {
